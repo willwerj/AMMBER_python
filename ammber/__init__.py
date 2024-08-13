47d94ef75d1011802de02639d63a20d5864008b6
Xander <102053371+fractalsbyx@users.noreply.github.com>
2024-08-13 12:14:22 -0400

    Update __init__.py

--- ammber/__init__.py
+++ ammber/__init__.py
@@ -4,6 +4,6 @@ ammber.
 A Python package for parametrizing gibbs free energy curves for phase-field modeling.
 """
 
-__version__ = "0.1.0"
-__author__ = 'Xander or Beck or Katsuyo'
+__version__ = "0.1.1"
+__author__ = 'W. Beck Andrews, Xander Mensah, Katsuyo Thornton'
 __credits__ = 'University of Michigan'
